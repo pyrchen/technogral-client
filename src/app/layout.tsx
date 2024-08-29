a6e2aa2ba9924ae4e23a03b1a2a7c51eb98030df
Pavel Yurchenko <pavel.yurchenko02@gmail.com>
2024-08-30 00:19:49 +0300

    feat: page container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
 	return (
 		<html lang='en'>
-			<body>
+			<body className={inter.className}>
 				<StyledComponentsRegistry>
 					<GlobalStyles />
 					<StyledComponentsThemeRegistry>
