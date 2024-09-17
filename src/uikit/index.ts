d918786f5c68961ba9e9828840d14d9eccec2d2a
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-09-17 23:12:12 +0300

    fixed index file

--- src/uikit/index.ts
+++ src/uikit/index.ts
@@ -3,10 +3,23 @@ import Avatar from './Avatar/Avatar';
 import Button from './Button/Button';
 import Divider from './Divider/Divider';
 import Dot from './Dot/Dot';
+import ProgressBar from './ProgressBar/ProgressBar';
 import RadioField from './RadioField/RadioField';
 import RadioFieldsGroup from './RadioFieldsGroup/RadioFieldsGroup';
 import SelectField from './SelectField/SelectField';
 import TextInput from './TextInput/TextInput';
 import TypoText from './TypoText/TypoText';
 
-export { AnySection, Avatar, Button, Divider, Dot, RadioField, RadioFieldsGroup, SelectField, TextInput, TypoText };
+export {
+	AnySection,
+	Avatar,
+	Button,
+	Divider,
+	Dot,
+	ProgressBar,
+	RadioField,
+	RadioFieldsGroup,
+	SelectField,
+	TextInput,
+	TypoText,
+};
