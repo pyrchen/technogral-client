f5f1bffa9fc26aa11345b753141c03a7a994ffe9
Pavel Yurchenko <pavel.yurchenko02@gmail.com>
2024-10-16 02:05:29 +0300

    Remove middleware

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from 'next/server';
-
-export async function middleware(request: NextRequest) {
-	const { pathname } = request.nextUrl;
-	const url = request.nextUrl.clone();
-
-	const refreshToken = request.cookies.get('refreshToken');
-}
